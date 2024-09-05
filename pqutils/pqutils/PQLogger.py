86d941c5b5c3c28f77819ed5cf33571bc15df0fc
Jay <120996986+majunggil@users.noreply.github.com>
2024-09-05 12:56:11 +0900

    Update PQLogger.py
    1.0.5 버전으로 업데이트

--- pqutils/pqutils/PQLogger.py
+++ pqutils/pqutils/PQLogger.py
@@ -3,61 +3,35 @@ import logging
 import logging.handlers
 from datetime import datetime
 
+name = datetime.now().strftime("%Y%m%d_%H%M%S")
+logger = logging.getLogger(name)
+formatter = logging.Formatter('[%(levelname)s] %(asctime)s > %(message)s')
+filename = os.path.join(os.getcwd(), f'{name}.log')
+fileMaxByte = 1024 * 1024 * 10  # 10MB
+fileHandler = logging.handlers.RotatingFileHandler(
+    filename, maxBytes=fileMaxByte, backupCount=10
+)
+streamHandler = logging.StreamHandler()
 
-DEF_DEBUG = 0
-DEF_RELEASE = 1
-DEF_LEVEL = DEF_DEBUG
+fileHandler.setFormatter(formatter)
+streamHandler.setFormatter(formatter)
 
-
-def Logger():
-    # 로거 인스턴스를 만든다
-    global logger
-    name = datetime.now().strftime("%Y%m%d_%H%M%S")
-    logger = logging.getLogger(name)
-
-    # 포매터를 만든다
-    formatter = logging.Formatter('[%(levelname)s] %(asctime)s > %(message)s')
-
-    # 스트림과 파일로 로그를 출력하는 핸들러를 각각 만든다.
-    filename = os.getcwd() + '/' + name + '.log'
-    fileMaxByte = 1024 * 1024 * 10  # 10MB
-    fileHandler = logging.handlers.RotatingFileHandler(filename, maxBytes=fileMaxByte,
-                                                       backupCount=10)  # 10개 파일, overwrite 됨
-    streamHandler = logging.StreamHandler()
-
-    # 각 핸들러에 포매터를 지정한다.
-    fileHandler.setFormatter(formatter)
-    streamHandler.setFormatter(formatter)
-
-    # 로거 인스턴스에 스트림 핸들러와 파일핸들러를 붙인다.
-    logger.addHandler(fileHandler)
-    logger.addHandler(streamHandler)
-    logger.setLevel(logging.DEBUG)
+logger.addHandler(fileHandler)
+logger.addHandler(streamHandler)
+logger.setLevel(logging.DEBUG)
 
 
-# Debug
-def logger_debug(msg):
-    if DEF_LEVEL == DEF_DEBUG:
-        logger.debug(msg)
+def debug(msg):
+    logger.debug(msg)
 
+def info(msg):
+    logger.info(msg)
 
-# Information
-def logger_info(msg):
-    if DEF_LEVEL == DEF_DEBUG:
-        logger.info(msg)
+def warning(msg):
+    logger.warning(msg)
 
-
-# Warming
-def logger_warning(msg):
-    if DEF_LEVEL == DEF_DEBUG:
-        logger.warning(msg)
-
-
-# Error
-def logger_error(msg):
+def error(msg):
     logger.error(msg)
 
-
-# Critical
-def logger_critical(msg):
+def critical(msg):
     logger.critical(msg)
