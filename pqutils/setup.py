f448db9a41143b956d4760e6618d50665bf86ce9
Jay <120996986+majunggil@users.noreply.github.com>
2024-09-05 12:57:17 +0900

    Update setup.py
    1.0.5 버전으로 업데이트

--- pqutils/setup.py
+++ pqutils/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='pqutils',
-    version='1.0.0',
+    version='1.0.5',
     description='A utils package for PyQt5',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='majunggil',
-    author_email='majunggil.work@gmailc.om',
+    author_email='majunggil.work@gmail.com',
     url='https://github.com/majunggil/PyQt5/tree/main/pqutils',
     packages=find_packages(),
     install_requires=[
