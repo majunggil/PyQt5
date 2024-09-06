1954133ae69878d2dc45821e1940f3ad701fbd2e
Jay <120996986+majunggil@users.noreply.github.com>
2024-09-06 09:47:02 +0900

    Update PQLogger.py
    1.0.6 버전으로 업데이트

--- pqutils/pqutils/PQLogger.py
+++ pqutils/pqutils/PQLogger.py
@@ -1,12 +1,16 @@
 import os
-import logging
 import logging.handlers
 from datetime import datetime
 
-name = datetime.now().strftime("%Y%m%d_%H%M%S")
+path = os.path.join(os.getcwd(), 'log') + os.path.sep
+if not os.path.exists(path):
+    os.makedirs(path)
+
+name = path + datetime.now().strftime("%Y%m%d_%H%M%S")
 logger = logging.getLogger(name)
 formatter = logging.Formatter('[%(levelname)s] %(asctime)s > %(message)s')
-filename = os.path.join(os.getcwd(), f'{name}.log')
+
+filename = name + '.log'
 fileMaxByte = 1024 * 1024 * 10  # 10MB
 fileHandler = logging.handlers.RotatingFileHandler(
     filename, maxBytes=fileMaxByte, backupCount=10
@@ -24,14 +28,18 @@ logger.setLevel(logging.DEBUG)
 def debug(msg):
     logger.debug(msg)
 
+
 def info(msg):
     logger.info(msg)
+
 
 def warning(msg):
     logger.warning(msg)
 
+
 def error(msg):
     logger.error(msg)
+
 
 def critical(msg):
     logger.critical(msg)
